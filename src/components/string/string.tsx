3390050a00530a3f18f30fc7a05540e28fd4a953
olgushe4ka <olgushe4ka@gmail.com>
2023-02-17 15:13:56 +0300

    string layout

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -1,10 +1,21 @@
 import React from "react";
+import { Button } from "../ui/button/button";
+import { Input } from "../ui/input/input";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+
+import styles from "./string.module.css";
+
 
 export const StringComponent: React.FC = () => {
   return (
     <SolutionLayout title="Строка">
-     
+      <div className={styles.stringbox}>
+        <div className={styles.inputbox}>
+        <div className={styles.input}> <Input  max={11}></Input> </div>
+          <Button text="Развернуть" />
+        </div>
+        Максимум 11 символов
+      </div>
     </SolutionLayout>
   );
 };
