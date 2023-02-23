a6fec23fe4da51db314748db14bf8beb6869adcc
olgushe4ka <olgushe4ka@gmail.com>
2023-02-23 21:15:46 +0300

    pages layout

--- src/components/list-page/list-page.tsx
+++ src/components/list-page/list-page.tsx
@@ -1,10 +1,122 @@
-import React from "react";
+import React, { ChangeEvent, useState } from "react";
+import { ElementStates } from "../../types/element-states";
+import { Button } from "../ui/button/button";
+import { Circle } from "../ui/circle/circle";
+import { ArrowIcon } from "../ui/icons/arrow-icon";
+import { Input } from "../ui/input/input";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 
+import styles from "./list.module.css";
+
+type TArray = {
+  value: string;
+  color: ElementStates;
+};
+
+const defaultArray = Array.from({ length: 4 }, () => ({
+  value: "1",
+  color: ElementStates.Default,
+}));
+
 export const ListPage: React.FC = () => {
+  const [valueInput, setValueInput] = useState<any>("");
+  const [array, setArray] = useState<TArray[]>(defaultArray);
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setValueInput(e.currentTarget.value);
+  };
+
+  const clickButtonAdd = () => {};
+
+  const clickButtonDel = () => {};
+
+  const clickButtonClear = () => {};
+
   return (
     <SolutionLayout title="Связный список">
+      <div className={styles.stringbox}>
+        <div className={styles.stringbox}>
+          <div className={styles.inputbox}>
+            <div className={styles.input}>
+              <Input max={11} onChange={onChange} value={valueInput}></Input>
+              <p className={styles.maxText}>Максимум 4 символа</p>
+            </div>
+            <div className={styles.btn}>
+              <Button
+                text="Добавить в head"
+                type="submit"
+                onClick={clickButtonAdd}
+                disabled={valueInput.length > 4}
+              />
+            </div>
+            <div className={styles.btn}>
+              <Button
+                text="Добавить в tail"
+                type="submit"
+                onClick={clickButtonDel}
+                disabled={valueInput.length > 4}
+              />
+            </div>
+            <div className={styles.btn}>
+              <Button
+                text="Удалить из head"
+                type="submit"
+                onClick={clickButtonAdd}
+                disabled={array.length == 0}
+              />
+            </div>
+            <div>
+              <Button
+                text="Удалить из tail"
+                type="submit"
+                onClick={clickButtonDel}
+                disabled={array.length == 0}
+              />
+            </div>{" "}
+          </div>
+        </div>
 
+        <div className={styles.stringboxSecond}>
+          <div className={styles.inputbox}>
+            <div className={styles.input}>
+              <Input max={11} onChange={onChange} value={valueInput}></Input>
+            </div>
+            <div className={styles.btnBig}>
+              <Button
+                text="Добавить по индексу"
+                type="submit"
+                onClick={clickButtonAdd}
+                disabled={valueInput.length > 4}
+                extraClass={styles.buttonWidth}
+              />
+            </div>
+            <div className={styles.btnBig}>
+              <Button 
+                text="Удалить по индексу"
+                type="submit"
+                onClick={clickButtonAdd}
+                disabled={array.length == 0}
+                extraClass={styles.buttonWidth}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <ul className={styles.curcles}>
+        {array.map((item, index) => (
+          <li className={styles.curcle} key={index}>
+            <div>
+              {index == array.length - 1 && <p className={styles.head}>head</p>}
+              <Circle letter={item.value} state={item.color} />
+
+              <p>{index}</p>
+              {index == array.length - 1 && <p>tail</p>}
+            </div>
+            {index !== array.length - 1 && <ArrowIcon />}
+          </li>
+        ))}
+      </ul>
     </SolutionLayout>
   );
 };
--- src/components/queue-page/queue-page.tsx
+++ src/components/queue-page/queue-page.tsx
@@ -1,10 +1,84 @@
-import React from "react";
+import React, { ChangeEvent, useState } from "react";
+import { ElementStates } from "../../types/element-states";
+import { Button } from "../ui/button/button";
+import { Circle } from "../ui/circle/circle";
+import { Input } from "../ui/input/input";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 
+import styles from "./queue.module.css";
+
+type TArray = {
+  value: string;
+  color: ElementStates;
+};
+
+const emptyArr = Array.from({ length: 7 }, () => ({
+  value: "",
+  color: ElementStates.Default,
+}));
+
 export const QueuePage: React.FC = () => {
+  const [valueInput, setValueInput] = useState<any>("");
+  const [array, setArray] = useState<TArray[]>(emptyArr);
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setValueInput(e.currentTarget.value);
+  };
+
+  const clickButtonAdd = () => {};
+
+  const clickButtonDel = () => {};
+
+  const clickButtonClear = () => {
+    setArray(emptyArr);
+  };
+
   return (
     <SolutionLayout title="Очередь">
+      <div className={styles.stringbox}>
+        <div className={styles.inputbox}>
+          <div className={styles.input}>
+            <Input max={11} onChange={onChange} value={valueInput}></Input>
+          </div>
+          <div>
+            <Button
+              text="Добавить"
+              type="submit"
+              onClick={clickButtonAdd}
+              disabled={valueInput === "" || valueInput.length > 4}
+            />
+          </div>{" "}
+          <div className={styles.btnDelete}>
+            <Button
+              text="Удалить"
+              type="submit"
+              onClick={clickButtonDel}
+              disabled={array.length == 0}
+            />
+          </div>{" "}
+          <div>
+            <Button
+              text="Очистить"
+              type="submit"
+              onClick={clickButtonClear}
+              disabled={array.length == 0}
+            />
+          </div>
+        </div>
+        Максимум 4 символа
+      </div>
 
+      <ul className={styles.curcles}>
+        {array.map((item, index) => (
+          <li className={styles.curcle} key={index}>
+            {index == array.length - 1 && <p className={styles.head}>head</p>}
+            <Circle letter={item.value} state={item.color} />
+
+            <p>{index}</p>
+            {index == array.length - 1 && <p>tail</p>}
+          </li>
+        ))}
+      </ul>
     </SolutionLayout>
   );
 };
