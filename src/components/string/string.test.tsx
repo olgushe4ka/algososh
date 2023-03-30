83d5730b4433669611d045ba608ddda0fe4b119d
olgushe4ka <olgushe4ka@gmail.com>
2023-03-30 14:35:48 +0300

    fixes

--- src/components/string/string.test.tsx
+++ src/components/string/string.test.tsx
@@ -1,27 +1,12 @@
 import { render, fireEvent, screen, getByRole } from "@testing-library/react";
-import { Link, Router } from "react-router-dom";
-import { StringComponent } from "./string";
-
-// import { createMemoryHistory } from "history";
 import App from "../app/app";
 
 describe("StringComponent", () => {
   it("correctly reverses a string with even number of characters", async () => {
-  
-   // const history = createMemoryHistory();
 
-
-
-   // render(<App />);
     const cont = render(<App />);
     const linkElementString = cont.getAllByRole("link");
     fireEvent.click(linkElementString[0]);
-
-    // render(
-    //   <Router history={history}>
-    //     <StringComponent />
-    //   </Router>
-    // );
 
     const input = screen.getByRole("textbox");
     const button = screen.getByRole("button", { name: /развернуть/i });
@@ -36,12 +21,12 @@ describe("StringComponent", () => {
   });
 
   it("correctly reverses a string with odd number of characters", async () => {
-    
-  const cont = render(<App />);
-  const linkElementString = cont.getAllByRole("link");
-  fireEvent.click(linkElementString[0]);
-    
+    const cont = render(<App />);
+    const linkElementString = cont.getAllByRole("link");
+    fireEvent.click(linkElementString[0]);
+
     const input = screen.getByRole("textbox");
+    // const input = document.getElementsByClassName("input");
     const button = screen.getByRole("button", { name: /развернуть/i });
 
     fireEvent.change(input, { target: { value: "abcde" } });
@@ -50,17 +35,16 @@ describe("StringComponent", () => {
     await new Promise((r) => setTimeout(r, 4000));
 
     const circles = await screen.findAllByRole("listitem");
-    expect(circles.map((c) => c.textContent)).toEqual(["e", "d", "c", "b", "a"]);
+    expect(circles.map((c) => c.textContent)).toEqual([
+      "e",
+      "d",
+      "c",
+      "b",
+      "a",
+    ]);
   });
 
   it("correctly reverses a string with one character", async () => {
-    
-    // const history = createMemoryHistory();
-    // render(
-    //   <Router history={history}>
-    //     <StringComponent />
-    //   </Router>
-    // );
 
     const cont = render(<App />);
     const linkElementString = cont.getAllByRole("link");
@@ -79,14 +63,6 @@ describe("StringComponent", () => {
   });
 
   it("correctly handles an empty string", async () => {
-    // const history = createMemoryHistory();
-   
-    // render(
-    //   <Router history={history}>
-    //     <StringComponent />
-    //   </Router>
-    // );
-
     const cont = render(<App />);
     const linkElementString = cont.getAllByRole("link");
     fireEvent.click(linkElementString[0]);
@@ -97,10 +73,6 @@ describe("StringComponent", () => {
     fireEvent.change(input, { target: { value: "" } });
     fireEvent.click(button);
 
-   
     expect(button).toBeDisabled();
-
-    // const circles = await screen.findAllByRole("listitem");
-    // expect(circles.map((c) => c.textContent)).toEqual([]);
-  });
+      });
 });
